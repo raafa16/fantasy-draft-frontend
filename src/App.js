d032952212dbd324222ddbba3b19327ea82f5016
raafa16 <raafa16@hotmail.com>
2020-04-12 01:56:34 -0400

    Login and Signup components. routing for new components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,89 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Dashboard from "./components/Dashboard";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.loginStatus();
+  }
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user,
+    });
+  };
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {},
+    });
+  };
+
+  loginStatus = () => {
+    axios
+      .get("http://localhost:3001/api/v1/logged_in", { withCredentials: true })
+      .then((response) => {
+        console.log(response);
+        if (response.data.logged_in) {
+          this.handleLogin(response);
+        } else {
+          this.handleLogout();
+        }
+      })
+      .catch((error) => console.log("api errors:", error));
+  };
+
   render() {
     return (
       <div className="App">
         <Router>
           <Switch>
-            <Route exact path={"/"} component={Home}></Route>
-            <Route exact path={"/dashboard"} component={Dashboard}></Route>
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Home
+                  {...props}
+                  handleLogout={this.handleLogout}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/login"
+              render={(props) => (
+                <Login
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/signup"
+              render={(props) => (
+                <Signup
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              )}
+            />
           </Switch>
         </Router>
       </div>
---
+++ src/components/auth/Signup.js
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+// import { axiosInstance } from "../../utils/API";
+import axios from "axios";
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      registrationErrors: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const { username, email, password, passwordConfirmation } = this.state;
+
+    axios
+      .post(
+        "http://localhost:3001/api/v1/registrations",
+        {
+          user: {
+            name: username,
+            email: email,
+            password: password,
+            password_confirmation: passwordConfirmation,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        if (response.status === 201) {
+          this.props.handleLogin(response.data);
+          this.redirect();
+        } else {
+          console.log(response);
+          this.setState({
+            errors: response.data.errors,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  redirect = () => {
+    this.props.history.push("/");
+  };
+
+  handleErrors = () => {
+    return (
+      <div>
+        <ul>
+          {this.state.errors.map((error, index) => {
+            return <li key={index}>{error}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            required
+          ></input>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          ></input>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          ></input>
+          <input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Password Confirmation"
+            value={this.state.passwordConfirmation}
+            onChange={this.handleChange}
+            required
+          ></input>
+
+          <button type="submit">Sign Up</button>
+        </form>
+        <div>{this.state.errors ? this.handleErrors() : null}</div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
